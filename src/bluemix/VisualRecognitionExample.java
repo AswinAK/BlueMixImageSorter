55addbf1cd9c779530e51220fbe2e3018752f74f
aanilku <aanilku@ncsu.edu>
2016-10-30 01:41:55 -0400

    added GUI

--- src/bluemix/VisualRecognitionExample.java
+++ src/bluemix/VisualRecognitionExample.java
@@ -1,5 +1,9 @@
 package bluemix;
 
+import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -12,6 +16,16 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.stream.Stream;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 import java.io.File;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifierOptions;
@@ -22,21 +36,110 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-public class VisualRecognitionExample {
+public class VisualRecognitionExample extends JPanel implements ActionListener {
+	
+	JButton openButton, sortButton;
+    JTextArea log;
+    JFileChooser fc;
+    File myFolder = null;
+	
+    
+    
+    public VisualRecognitionExample() {
+    	super(new BorderLayout());
+    	
+    	log = new JTextArea(5,20);
+        log.setMargin(new Insets(5,5,5,5));
+        log.setEditable(false);
+        JScrollPane logScrollPane = new JScrollPane(log);
+
+        fc = new JFileChooser();
+
+        //fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        openButton = new JButton("Load Folder...");
+        openButton.addActionListener(this);
+
+        sortButton = new JButton("Sort Images");
+        sortButton.addActionListener(this);
+
+        //For layout purposes, put the buttons in a separate panel
+        JPanel buttonPanel = new JPanel(); //use FlowLayout
+        buttonPanel.add(openButton);
+        buttonPanel.add(sortButton);
+
+        //Add the buttons and the log to this panel.
+        add(buttonPanel, BorderLayout.PAGE_START);
+        add(logScrollPane, BorderLayout.CENTER);
+    }
+    
+    @Override
+	public void actionPerformed(ActionEvent e) {
+		//Handle open button action.
+        if (e.getSource() == openButton) {
+        	myFolder = null;
+            int returnVal = fc.showOpenDialog(VisualRecognitionExample.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                myFolder = fc.getSelectedFile();
+                //This is where a real application would open the file.
+                log.setText("");
+                log.append("Load: " + myFolder.getAbsolutePath() + "\n");
+            } 
+            else {
+                log.append("Open command cancelled by user.\n");
+            }
+            log.setCaretPosition(log.getDocument().getLength());
+
+        //Handle sort button action.
+        } 
+        if (e.getSource() == sortButton) {
+        	if(myFolder != null) {
+        		log.append("Sorting: " + myFolder.getName() + ".\n");
+        		//TODO BLUEMIX
+        		organizePictures();
+        	}
+        	else {
+        		log.append("Error: No directory loaded.");
+        	}
+            log.setCaretPosition(log.getDocument().getLength());
+        }
+	}
+	
+	private static void createAndShowGUI() {
+		JFrame frame = new JFrame("HackNC");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new VisualRecognitionExample());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+	}
+    
 	private static String InputPath = "/Users/AswinAk/Desktop/BlueMixTest";
 
 	public static void main(String[] args) {
 		
+	    SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	                //Turn off metal's use of bold fonts
+	                UIManager.put("swing.boldMetal", Boolean.FALSE); 
+	                createAndShowGUI();
+	            }
+	        });
+		
+	}	
+	public static void organizePictures(){
 		ArrayList<String> filePaths = new ArrayList<String>();
 		ArrayList<Image> imageCollection =  new ArrayList<Image>();
 		Set<String> uniqueCategories = new HashSet<String>();
 		File file = null;
 		FileChannel inputChannel,outputChannel;
 		String source,dest;
-		
+
 		VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
 		service.setApiKey("9e3a667265d8ad82e2d01cf2502ede9727e74e96");
-		
+
 		try(Stream<Path> paths = Files.walk(Paths.get(InputPath))) {
 		    paths.forEach(filePath -> {
 		        if (Files.isRegularFile(filePath)) {
@@ -49,33 +152,30 @@ public class VisualRecognitionExample {
 			e.printStackTrace();
 		}
 
-		int i =1;
 		for(String path: filePaths){
 			ClassifyImagesOptions options = new ClassifyImagesOptions.Builder()
 					.images(new File(path)).build();
 			VisualClassification result = service.classify(options).execute();
-			System.out.println("Analysis for image "+i);
-			System.out.println(result);
+			//System.out.println(result);
 			String category = result.getImages().get(0).getClassifiers().get(0).getClasses().get(0).getName();
 			Image image = new Image(path, category);
 			imageCollection.add(image);
 			uniqueCategories.add(category);
 		}
-		
+
 		System.out.println("Set is "+uniqueCategories);
-		
+
 		Iterator<String> setIterator  = uniqueCategories.iterator();
-		
+
 		while(setIterator.hasNext()){
 			String currentCategory = setIterator.next();	
 			file = new File(InputPath+"/"+currentCategory);
 			if (!file.exists())
 				file.mkdir();
 		}
-		
+
 		for(Image img: imageCollection){
-		     inputChannel = null;
-			 outputChannel = null;
+		     inputChannel = null;outputChannel = null;
 			 source = img.getPath();
 			 dest = InputPath+"/"+img.getCategory();
 			try {
@@ -85,15 +185,23 @@ public class VisualRecognitionExample {
 				inputChannel.close();
 				outputChannel.close();
 			} 
-			catch(Exception e){
-				
-			}
-			
-			
-			
-			
+			catch(Exception e){			
+			}		
 		}
 		
-
+		
+		
 	}
+	    
+	    
+	    
+	    
+	
 }
+
+
+
+
+
+
+
